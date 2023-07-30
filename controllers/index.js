201133025ed4f682d5b1dbe02265cbb765cc7662
Michael Rose <michael@rosenetwork.net>
2023-07-30 12:24:47 -0700

    removed unneeded work around

--- controllers/index.js
+++ controllers/index.js
@@ -5,28 +5,20 @@ const Request = require('../models/request')
 const User = require('../models/user')
 const Location = require('../models/location')
 async function index(req, res) {
-  const possibleLocations = await Location.find({})
   if (req.user){
-    // we will have to herein invalidate the cookie locationId if user is not at location any longer
     const myRequests =await Request.find({requester: req.user._id, status: 'active'}).populate('requester')
     const peopleHere = await  User.find({})
-    if (!req.cookies.locationId){
-      res.redirect("/locations?target=/?method=GET")
-      return
-    }
     const location = await Location.findOne({_id: req.cookies.locationId})
   res.render("index", {
     title: "Humanhelp",
     requests: myRequests,
     users: peopleHere ,
-    possibleLocations: possibleLocations,
     locationName: location.name,
     id: myRequests.length > 0 ? myRequests[0]._id : -1
   });
   } else {
       res.render("index", {
       title: "Humanhelp",
-      possibleLocations: possibleLocations,
       locationName : '.',
       requests: [],
       users: [],
